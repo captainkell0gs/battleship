8687119dfa893e217eb04d0713a51e3602940e7b
captainkell0gs <captainkell0gs01@gmail.com>
2026-02-23 17:14:13 +0100

    feat(ship): Implement Ship class with hit() and isSunk()
    - Implement hit() to increment hit counter
    - Implement isSunk() based on length
    - Add unit tests covering hit behavior and sinking logic

---
+++ tests/ship.test.js
@@ -0,0 +1,21 @@
+import Ship from '../src/ship';
+
+describe('ship', () => {
+    let ship;
+
+    beforeEach(() => {
+        ship = new Ship(3);
+    })
+
+    test('should create a ship with the given length', () => {
+        expect(ship.length).toBe(3);
+        expect(ship.hits).toBe(0);
+    })
+
+    test('should increment when hits is called', () => {
+        ship.hit();
+        ship.hit();
+
+        expect(ship.hits).toBe(2);
+    })
+} )
